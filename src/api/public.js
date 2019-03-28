935326f5c358f24c4a6c7ee4262e72665c356d04
Father <18918067039@163.com>
2019-03-28 19:51:52 +0900

    登陆注册菜单购物车通了

--- src/api/public.js
+++ src/api/public.js
@@ -1,17 +1,29 @@
 import axios from 'axios'
+
 axios.defaults.timeout = 10000
 axios.defaults.headers.post['Content-Type'] = 'application/x-www=form-urlencoded'
 export default {
-  fetchGet (url, params = {}) {
+  fetchGet (url, params) {
     return new Promise((resolve, reject) => {
       axios.get(url, params).then(res => {
         resolve(res.data)
+        console.log(res)
       }).catch(error => {
         reject(error)
       })
     })
   },
-  fetchPost (url, params = {}) {
+  // fetchGet (url, params) {
+  //   axios.get(url, params)
+  //     .then(function (response) {
+  //       console.log(response)
+  //     })
+  //     .catch(function (error) {
+  //       console.log(error)
+  //     })
+  // },
+
+  fetchPost (url, params) {
     return new Promise((resolve, reject) => {
       axios.post(url, params).then(res => {
         resolve(res.data)
--- src/components/mallGoods.vue
+++ src/components/mallGoods.vue
@@ -2,24 +2,24 @@
   <div class="good-item">
     <div style="">
       <div class="good-img">
-        <a @click="openProduct(msg.productId)">
-          <img v-lazy="msg.productImageBig" :alt="msg.productName" :key="msg.productImageBig">
+        <a @click="openProduct(msg.id)">
+          <img v-lazy="msg.productImageBig" :alt="msg.name" :key="msg.productImageBig">
         </a>
       </div>
-      <h6 class="good-title" v-html="msg.productName">{{msg.productName}}</h6>
-      <h3 class="sub-title ellipsis">{{msg.subTitle}}</h3>
+      <h6 class="good-title" v-html="msg.name">{{msg.name}}</h6>
+      <h3 class="sub-title ellipsis">{{msg.introduce}}</h3>
       <div class="good-price pr">
         <div class="ds pa">
-          <a @click="openProduct(msg.productId)">
+          <a @click="openProduct(msg.id)">
             <y-button text="查看详情" style="margin: 0 5px"></y-button>
           </a>
           <y-button text="加入购物车"
                     style="margin: 0 5px"
-                    @btnClick="addCart(msg.productId,msg.salePrice,msg.productName,msg.productImageBig)"
+                    @btnClick="addCart(msg.id,msg.prize,msg.name,msg.productImageBig)"
                     classStyle="main-btn"
           ></y-button>
         </div>
-        <p><span style="font-size:14px">￥</span>{{Number(msg.salePrice).toFixed(2)}}</p>
+        <p><span style="font-size:14px">￥</span>{{Number(msg.prize).toFixed(2)}}</p>
       </div>
     </div>
   </div>
